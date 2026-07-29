5f6791af89818ac32fe4513f5beac80104fde74a
czfsdg <129081062+czfsdg@users.noreply.github.com>
2026-07-29 20:15:43 +0800

    Update dcut_causal_conv1d_infershape.cpp
    Signed-off-by: czfsdg <129081062+czfsdg@users.noreply.github.com>

--- dcut/kernel/dcut_causal_conv1d/op_host/dcut_causal_conv1d_infershape.cpp
+++ dcut/kernel/dcut_causal_conv1d/op_host/dcut_causal_conv1d_infershape.cpp
@@ -1,26 +1,2 @@
-// SPDX-License-Identifier: Apache-2.0
-
-#include "register/op_impl_registry.h"
-#include "tiling_base/error_log.h"
-
-using namespace ge;
-
-namespace ops {
-namespace {
-constexpr int64_t TENSOR_INDEX = 0;
-
-ge::graphStatus InferShapeDcutCausalConv1d(
-    gert::InferShapeContext* context) {
-  const gert::Shape* x_shape = context->GetInputShape(TENSOR_INDEX);
-  OP_CHECK_NULL_WITH_CONTEXT(context, x_shape);
-
-  gert::Shape* y_shape = context->GetOutputShape(TENSOR_INDEX);
-  OP_CHECK_NULL_WITH_CONTEXT(context, y_shape);
-  *y_shape = *x_shape;
-  return GRAPH_SUCCESS;
-}
-}  // namespace
-
-IMPL_OP_INFERSHAPE(DcutCausalConv1d)
-    .InferShape(InferShapeDcutCausalConv1d);
-}  // namespace ops
+#define CausalConv1d DcutCausalConv1d
+#include "../../../../csrc/moe/causal_conv1d/op_host/causal_conv1d_infershape.cpp"
